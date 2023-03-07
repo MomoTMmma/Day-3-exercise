ccb3b548ac299648eb0e8de339f40c434e7f293d
Melissa Arreola <melissaarreola@melissas-mbp.lan>
2023-03-06 21:03:40 -0800

    first

---
+++ melissa_module.py
@@ -0,0 +1,9 @@
+# 1)Has a function to calculate the square footage of a house
+# Reminder of Formula: Length X Width == Area
+
+# 2)Has a function to calculate the circumference of a circle
+
+# Program in Jupyter Notebook should take in user input and use imported functions to calculate a circle's circumference or a houses square footage
+import math
+def square():
+    for i in math.pi
